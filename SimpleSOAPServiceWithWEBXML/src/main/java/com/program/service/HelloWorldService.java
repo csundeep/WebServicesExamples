fceaada04ca746404a02a25372a2b032f8da1bed
sandychowdary.535 <sandychowdary.535@gmail.com>
2016-04-13 14:40:32 -0500

    Added @WebResult

--- SimpleSOAPServiceWithWEBXML/src/main/java/com/program/service/HelloWorldService.java
+++ SimpleSOAPServiceWithWEBXML/src/main/java/com/program/service/HelloWorldService.java
@@ -2,6 +2,7 @@ package com.program.service;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
@@ -11,5 +12,6 @@ import javax.jws.soap.SOAPBinding.Use;
 @SOAPBinding(style = Style.RPC, use = Use.LITERAL)
 public interface HelloWorldService {
 	@WebMethod
+	@WebResult(name="welcome")
 	String sendMessage(@WebParam(name = "message") String message);
 }
